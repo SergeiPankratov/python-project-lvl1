2ddab4a4a4c463736fd652c889ca54ac6bc30dd1
SergeiPankratov <pankratovsergei96@gmail.com>
2021-10-12 16:05:59 +0300

    update calc_logic.py

--- brain_games/games/calc_logic.py
+++ brain_games/games/calc_logic.py
@@ -5,6 +5,19 @@ import random
 import brain_games.games.cli
 
 
+def mechanic_game(number_one, number_two, operator):
+    if operator == '+':
+        result = str(number_one + number_two)
+
+    elif operator == '-':
+        result = str(number_one - number_two)
+
+    elif operator == '*':
+        result = str(number_one * number_two)
+
+    return result
+
+
 def calculate():
     name = brain_games.games.cli.welcome_user()
     print('What is the result of the expression?')
@@ -17,38 +30,14 @@ def calculate():
         expression = str(f'{number_one} {operator} {number_two}')
         print(f'Question: {expression}')
         answer = input('Your answer: ')
-        if operator == '+':
-            result = str(number_one + number_two)
-
-            if answer == result:
-                counter += 1
-                print('Correct!')
-            else:
-                counter += 4
-                print(f"'{answer}' is wrong answer ;(.", end = '')
-                print(f"Correct answer was '{result}'")
-
-        elif operator == '-':
-            result = str(number_one - number_two)
-
-            if answer == result:
-                counter += 1
-                print('Correct!')
-            else:
-                counter += 4
-                print(f"'{answer}' is wrong answer ;(.", end = '')
-                print(f"Correct answer was '{result}'")
-
-        elif operator == '*':
-            result = str(number_one * number_two)
-
-            if answer == result:
-                counter += 1
-                print('Correct!')
-            else:
-                counter += 4
-                print(f"'{answer}' is wrong answer ;(.", end = '')
-                print(f"Correct answer was '{result}'")
+        result = mechanic_game(number_one, number_two, operator)
+        if answer == result:
+            counter += 1
+            print('Correct!')
+        else:
+            counter += 4
+            print(f"'{answer}' is wrong answer ;(.", end='')
+            print(f"Correct answer was '{result}'")
 
     if counter == 3:
         print(f'Congratulations, {name}!')
